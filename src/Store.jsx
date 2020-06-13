0622bd44dc78a0181f3a4461111e6cdcbe3079a0
SedationH <sedationh@gmail.com>
2020-06-13 22:34:37 +0800

    fetch API

--- src/Store.jsx
+++ src/Store.jsx
@@ -1,13 +1,41 @@
-import React from "react";
+import React, { useReducer, useEffect } from "react";
 
 export const Store = React.createContext();
 
-const initialState = {};
+const initialState = {
+  episodes: [],
+  favourites: []
+};
 
-function reducer() {}
+function reducer(state, action) {
+  switch (action.type) {
+    case "FETCH_DATA":
+      return { ...state, episodes: action.payload };
+    default:
+      return state;
+  }
+}
+
+/**
+ * action -> reducer -> store -> UI
+ */
 
 export function StoreProvider(props) {
-  return (
-    <Store.Provider value="date from store">{props.children}</Store.Provider>
-  );
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const fetchDataAction = async () => {
+    const data = await fetch(
+      "https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty&embed=episodes"
+    );
+    const dataJSON = await data.json();
+    dispatch({
+      type: "FETCH_DATA",
+      payload: dataJSON._embedded.episodes
+    });
+  };
+  useEffect(() => {
+    state.episodes.length === 0 && fetchDataAction();
+  });
+  const value = { state, dispatch };
+  return <Store.Provider value={value}>{props.children}</Store.Provider>;
 }
